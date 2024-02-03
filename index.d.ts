b2159da5a0f1dfc97ec2d42a4910ce97fb2a5900
Alpha5959 <109584578+Alpha5959@users.noreply.github.com>
2024-02-03 22:13:03 +0600

    Update index.d.ts (v1.3.0)

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,11 @@
-import type { Client } from 'discord.js';
+import { Client } from "discord.js";
 
 interface ErrorHandlingConfig {
   webhookUrl: string;
   embedColor?: string;
   embedTitle?: string;
   webhookUsername?: string;
+  embedAvatarUrl?: string;
 }
 
 declare function errorHandling(
